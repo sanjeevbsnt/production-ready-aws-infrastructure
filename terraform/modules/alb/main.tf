b8da72f20dc36cab63ff75487b33b66f79989654
Sanjeev Basnet <sanjeev.basnet@genesesolution.com>
2026-04-26 08:36:21 +0545

    Updated ALB

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -1,7 +1,10 @@
 resource "aws_security_group" "alb" {
-  vpc_id = var.vpc_id
+  name        = "${var.project_name}-alb-sg"
+  description = "Security group for ALB"
+  vpc_id      = var.vpc_id
 
   ingress {
+    description = "Allow HTTP from internet"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -9,10 +12,15 @@ resource "aws_security_group" "alb" {
   }
 
   egress {
+    description = "Allow outbound traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-alb-sg"
   }
 }
 
@@ -21,12 +29,43 @@ resource "aws_lb" "this" {
   load_balancer_type = "application"
   subnets            = var.public_subnets
   security_groups    = [aws_security_group.alb.id]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "${var.project_name}-alb"
+  }
 }
 
 resource "aws_lb_target_group" "this" {
-  name     = "${var.project_name}-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
+  name        = "${var.project_name}-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
   target_type = "ip"
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "${var.project_name}-tg"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
 }
--- terraform/modules/alb/outputs.tf
+++ terraform/modules/alb/outputs.tf
@@ -1,11 +1,11 @@
-output "alb_sg_id" {
-  value = aws_security_group.alb.id
+output "alb_dns" {
+  value = aws_lb.this.dns_name
 }
 
 output "target_group_arn" {
   value = aws_lb_target_group.this.arn
 }
 
-output "alb_dns" {
-  value = aws_lb.this.dns_name
+output "alb_security_group_id" {
+  value = aws_security_group.alb.id
 }
